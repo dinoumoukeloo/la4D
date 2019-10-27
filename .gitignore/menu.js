4de59afa33f0c02afae77d7c8af222f3820a44ad
dinoumoukeloo <56556162+dinoumoukeloo@users.noreply.github.com>
2019-10-27 19:50:44 +0100

    Create menu.js

---
+++ .gitignore/menu.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+
+
+var perfix = ".tt"
+
+
+
+client.on('message', message => {
+
+  if (message.content === 'ping' ) {
+
+    message.reply('pong !')
+
+  }
+
+})
+
+
+
+client.on("ready", () => {
+
+  console.log("Ready");
+
+  client.user.setGame("★☆4D☆★");
+
+})
+
+
+
+
+
+
+
+client.login(process.env.TOKEN)
